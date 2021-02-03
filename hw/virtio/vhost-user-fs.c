ecac25516cab593bf017d3887dbcd9ffd50acf3d
Vivek Goyal <vgoyal@redhat.com>
2021-02-03 11:56:55 -0500

    vhost-user-fs: Extend VhostUserFSSlaveMsg to pass additional info
    Extend VhostUserFSSlaveMsg so that slave can ask it to drop CAP_FSETID
    before doing I/O on fd.
    
    In some cases, virtiofsd takes the onus of clearing setuid bit on a file
    when WRITE happens. Generally virtiofsd does the WRITE to fd (from guest
    memory which is mapped in virtiofsd as well), but if this memory is
    unmappable in virtiofsd (like cache window), then virtiofsd asks qemu
    to do the I/O instead.
    
    To retain the capability to drop suid bit on write, qemu needs to
    drop the CAP_FSETID as well before write to fd. Extend VhostUserFSSlaveMsg
    so that virtiofsd can specify in message if CAP_FSETID needs to be
    dropped.
    
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>

--- hw/virtio/vhost-user-fs.c
+++ hw/virtio/vhost-user-fs.c
@@ -244,6 +244,11 @@ uint64_t vhost_user_fs_slave_io(struct vhost_dev *dev, size_t message_size,
         return (uint64_t)-1;
     }
 
+    if (sm->hdr.flags & VHOST_USER_FS_GENFLAG_DROP_FSETID) {
+        error_report("Dropping CAP_FSETID is not supported");
+        return (uint64_t)-ENOTSUP;
+    }
+
     for (i = 0; i < sm->hdr.count && !res; i++) {
         VhostUserFSSlaveMsgEntry *e = &sm->entries[i];
         if (e->len == 0) {
