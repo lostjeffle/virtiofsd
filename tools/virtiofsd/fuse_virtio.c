rhvgoyal/qemu

DAX: libvhost-user: Allow popping a queue element with bad pointers
Allow a daemon implemented with libvhost-user to accept an
element with pointers to memory that aren't in the mapping table.
The daemon might have some special way to deal with some special
cases of this.

The default behaviour doesn't change.

Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

contrib/virtiofsd/fuse_virtio.c
@@ -496,7 +496,8 @@ static void *fv_queue_thread(void *opaque)
              * scatter/gather set and we can't trust the guest to keep them
              * still; so copy in/out.
              */
-            VuVirtqElement *elem = vu_queue_pop(dev, q, sizeof(VuVirtqElement));
+            VuVirtqElement *elem = vu_queue_pop(dev, q, sizeof(VuVirtqElement),
+                                                NULL, NULL);
             if (!elem) {
                 break;
             }

lostjeffle/virtiofsd

Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>

tools/virtiofsd/fuse_virtio.c
@@ -697,7 +697,8 @@ static void *fv_queue_thread(void *opaque)
                  __func__, qi->qidx, (size_t)evalue, in_bytes, out_bytes);
 
         while (1) {
-            FVRequest *req = vu_queue_pop(dev, q, sizeof(FVRequest));
+            FVRequest *req = vu_queue_pop(dev, q, sizeof(FVRequest),
+                                          NULL, NULL);
             if (!req) {
                 break;
             }